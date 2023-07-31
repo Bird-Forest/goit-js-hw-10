66836c7319a79c46742340c1238a807308bd8c6b
Bird-Forest <bird.forest70@gmail.com>
2023-07-31 19:14:45 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-const loaderImg = 'url(https://papik.pro/grafic/uploads/posts/2023-04/1682206914_papik-pro-p-stikeri-rizhii-kot-vektor-26.png)';
+const api_key = `live_CIw3lZRkcpgh759C9YBXivIvAETipzFqRyXtOHa4sXukf5xIGdNG9JZOQ72DPlKH`;
 
 import './css/styles.css';
-
 import { fetchBreeds } from "./js/cat-api";
 
 const errorItem = document.querySelector('.error');
-errorItem.textContent = '';
-errorItem.style.display = 'none';
 const loaderItem = document.querySelector('.loader');
-
 const catInfo = document.querySelector('.cat-info');
 const select = document.querySelector('.breed-select');
+
 select.addEventListener('change', fetchCatByBreed);
-select.style.visibility = 'hidden';
+
+errorItem.style.display = 'none';
+select.style.display = 'none';
 
 let storedBreeds = [];
 
@@ -20,7 +19,7 @@ fetchBreeds()
     .then((data) => {
         data = data.filter(img => img.image?.url != null)
         storedBreeds = data;
-    
+        
         for (let i = 0; i < storedBreeds.length; i++) {
             const breed = storedBreeds[i];
             let option = document.createElement('option');
@@ -32,12 +31,9 @@ fetchBreeds()
         };
     })
     .catch((error) => console.log(error)); 
-        
-const api_key = `live_CIw3lZRkcpgh759C9YBXivIvAETipzFqRyXtOHa4sXukf5xIGdNG9JZOQ72DPlKH`
 
 function fetchCatByBreed() {
-    let breedId = select.value
-    
+    let breedId = select.value 
     const url = "https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}"
     
     return fetch(url, {
@@ -47,17 +43,34 @@ function fetchCatByBreed() {
     })
         .then((response) => {
             if (!response.ok) {
-                throw new Error(response.status);
-            } 
+                throw errorMessage();
+            }
             return response.json();
         })
+
         .then((data) => {
-            let id = select.selectedIndex
+            let id = select.selectedIndex;
+
             data = storedBreeds[id];
-        
+            
             return renderBreeds();
         })
-        .catch((error) => errorMessage());
+        .catch((error) => console.log(error));
+};
+
+function onLoader() {
+
+    select.style.display = 'block';
+   
+    fetchCatByBreed();
+};
+onLoader()
+
+function errorMessage() {
+
+    loaderItem.style.display = 'none';
+
+    errorItem.style.display = 'block';
 };
 
 function renderBreeds() { 
@@ -75,12 +88,11 @@ function renderBreeds() {
     image.src = `${storedBreeds[id].image.url}`;
     image.style.width = `${700}px`;
     image.style.height = `${600}px`;
-    image.style.backgroundSize = 'cover';
+    image.style.objectFit = 'cover';
 
     title.textContent = `${storedBreeds[id].name}`;
     title.style.fontSize = `${38}px`;
     title.style.marginBottom = 0;
-    title.style.backgroundColor = 'rgb(255, 255, 255)';
 
     description.textContent = `${storedBreeds[id].description}`;
     description.style.fontSize = `${24}px`;
@@ -90,26 +102,32 @@ function renderBreeds() {
     temperament.style.marginTop = 0;
 
     catInfo.append(image, title, description, temperament);
-};
-
-function onLoader() {
-
-    select.style.visibility = 'visible';
-   
-    fetchCatByBreed();
-};
-
-onLoader()
-
-function errorMessage() {
 
     loaderItem.style.display = 'none';
-    loaderItem.textContent = '';
-    errorItem.textContent = 'Oops! Something went wrong! Try reloading the page!';
-    errorItem.style.display = 'block';
-
 };
-// // errorMessage()
+
+
+
+
+
+
+
+
+
+// select.style.visibility = 'visible';
+// select.style.visibility = 'hidden';
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // const errorItem = document.querySelector('.error')
@@ -121,19 +139,3 @@ function errorMessage() {
 
 
 // catInfo.innerHTML +=`<img src="${image.src}" alt=""><h2>${title.textContent}</h2><p>${description.textContent}</p><h3>${temperament.textContent}</h3>`
-
-
-// const boxUrl = document.createElement('div')
-// errorItem.after(boxUrl);
-// boxUrl.style.display = 'block';
-// boxUrl.style.width = `${700}px`;
-// boxUrl.style.height = `${700}px`;
-// boxUrl.style.backgroundImage = 'url(https://t3.ftcdn.net/jpg/00/61/67/86/240_F_61678606_bNPFDZsyh2tG7Lxjm5Jd0Xd7QMeumObE.jpg)'
-// boxUrl.style.backgroundRepeat = 'no-repeat';
-
-// errorItem.style.display = 'none';
-    // errorItem.textContent = '';
-    // loaderItem.textContent = 'Loading data, please wait...';
-    // loaderItem.style.display = 'block'
-    // loaderItem.classList.replace('error', 'loader');
-    
