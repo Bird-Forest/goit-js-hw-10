3f48a07222c2f1d040871699df0f3ee2799c5f2f
Bird-Forest <bird.forest70@gmail.com>
2023-08-02 23:24:08 +0300

    fixed error

--- src/index.js
+++ src/index.js
@@ -7,25 +7,25 @@ const errorItem = document.querySelector('.error');
 const loaderItem = document.querySelector('.loader');
 const catInfo = document.querySelector('.cat-info');
 const select = document.querySelector('.breed-select');
+console.dir(select)
 
 select.addEventListener('change', fetchCatByBreed);
 
 errorItem.style.display = 'none';
 select.style.display = 'none';
+select.value = '';
 
 let storedBreeds = [];
 
 fetchBreeds()
     .then((data) => {
-        data = data.filter(img => img.image?.url != null)
         storedBreeds = data;
         
         for (let i = 0; i < storedBreeds.length; i++) {
+
             const breed = storedBreeds[i];
-            let option = document.createElement('option');
+            let option = document.createElement('option');         
             
-            if (!breed.image) continue
-            option.value = breed.id;
             option.innerHTML = `${breed.name}`;
             select.appendChild(option);
         };
@@ -47,19 +47,23 @@ function fetchCatByBreed() {
             }
             return response.json();
         })
-
-        .then((data) => {
-            let id = select.selectedIndex;
-
-            data = storedBreeds[id];
-            
-            return renderBreeds();
-        })
-        .catch((error) => console.log(error));
-};
+        
+    .then((data) => {
+        let id = select.selectedIndex;
+        data = storedBreeds[id];
+        
+        const obj = {
+            name: data.name,
+            description: data.description,
+            temperament: data.temperament,
+            image: data.image.url
+        }
+                return renderBreeds(obj);
+    })
+    .catch((error) => console.log(error));
+}
 
 function onLoader() {
-
     select.style.display = 'block';
    
     fetchCatByBreed();
@@ -67,46 +71,134 @@ function onLoader() {
 onLoader()
 
 function errorMessage() {
-
     loaderItem.style.display = 'none';
 
     errorItem.style.display = 'block';
 };
 
-function renderBreeds() { 
+function renderBreeds({ image, title, description, temperament}) { 
     let id = select.selectedIndex;
-
+    
     if (storedBreeds.length > 1) {
         catInfo.innerHTML = '';
-    }
-    const image = document.createElement('img');
-    const title = document.createElement('h2');
-    const description = document.createElement('p');
-    const temperament = document.createElement('h3');
+    };
 
-    image.style.display = 'block';
-    image.src = `${storedBreeds[id].image.url}`;
-    image.style.width = `${700}px`;
-    image.style.height = `${600}px`;
-    image.style.objectFit = 'cover';
-
-    title.textContent = `${storedBreeds[id].name}`;
-    title.style.fontSize = `${38}px`;
-    title.style.marginBottom = 0;
-
-    description.textContent = `${storedBreeds[id].description}`;
-    description.style.fontSize = `${24}px`;
-    
-    temperament.textContent = `Temperament: ${storedBreeds[id].temperament}`;
-    temperament.style.fontSize = `${28}px`;
-    temperament.style.marginTop = 0;
-
-    catInfo.append(image, title, description, temperament);
+    catInfo.innerHTML = `<img src="${image}" alt="" class = "picture">
+    <h2 class = "title">${title}</h2>
+    <p class = "description">${description}</p>
+    <h3 class = "temperament">${temperament}</h3>`
 
     loaderItem.style.display = 'none';
 };
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetchBreeds()
+//     .then((data) => {
+//         data = data.filter(img => img.image?.url != null)
+//         storedBreeds = data;
+        
+//         for (let i = 0; i < storedBreeds.length; i++) {
+//             const breed = storedBreeds[i];
+//             let option = document.createElement('option');
+            
+//             if (!breed.image) continue
+//             option.value = breed.id;
+//             option.innerHTML = `${breed.name}`;
+//             select.appendChild(option);
+//         };
+//     })
+//     .catch((error) => console.log(error)); 
+
+// function fetchCatByBreed() {
+//     let breedId = select.value 
+//     const url = "https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}"
+    
+//     return fetch(url, {
+//         headers: {
+//             'x-api-key': api_key
+//         }
+//     })
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw errorMessage();
+//             }
+//             return response.json();
+//         })
+
+//         .then((data) => {
+//             let id = select.selectedIndex;
+
+//             data = storedBreeds[id];
+            
+//             return renderBreeds();
+//         })
+//         .catch((error) => console.log(error));
+// };
+
+// function onLoader() {
+
+//     select.style.display = 'block';
+   
+//     fetchCatByBreed();
+// };
+// onLoader()
+
+// function errorMessage() {
+
+//     loaderItem.style.display = 'none';
+
+//     errorItem.style.display = 'block';
+// };
+
+// function renderBreeds() { 
+//     let id = select.selectedIndex;
+
+//     if (storedBreeds.length > 1) {
+//         catInfo.innerHTML = '';
+//     }
+//     const image = document.createElement('img');
+//     const title = document.createElement('h2');
+//     const description = document.createElement('p');
+//     const temperament = document.createElement('h3');
+
+//     image.style.display = 'block';
+//     image.src = `${storedBreeds[id].image.url}`;
+//     image.style.width = `${700}px`;
+//     image.style.height = `${600}px`;
+//     image.style.objectFit = 'cover';
+
+//     title.textContent = `${storedBreeds[id].name}`;
+//     title.style.fontSize = `${38}px`;
+//     title.style.marginBottom = 0;
+
+//     description.textContent = `${storedBreeds[id].description}`;
+//     description.style.fontSize = `${24}px`;
+    
+//     temperament.textContent = `Temperament: ${storedBreeds[id].temperament}`;
+//     temperament.style.fontSize = `${28}px`;
+//     temperament.style.marginTop = 0;
+
+//     catInfo.append(image, title, description, temperament);
+
+//     loaderItem.style.display = 'none';
+// };
 
 
 
